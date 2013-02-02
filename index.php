8078119ac86294f070efcb90342114fb2a6e0d16
unknown <Bernardo@Bernardo-PC.(none)>
2013-02-02 23:25:48 +0000

    calendar

--- index.php
+++ index.php
@@ -214,6 +214,49 @@ $app_name = idx($app_info, 'name', '');
       <p>Bem-Vindo à aplicação <span>DA MELHOR TURMA DO MUNDO</span>!</p>
      
     </section>
+	<?php
+// Primeiro dia do mes
+$primeiro_dia = mktime(0, 0, 0, 1, 1);
+ 
+// Numero de dias no mes corrente
+$num_dias = date('t', $primeiro_dia);
+ 
+// O primeiro dia cai no dia da semana
+$pri_dia_sem = date('w', $primeiro_dia);
+ 
+/**
+* Digamos que neste mes X o primeiro dia começa na Quarta e tem 30 dias
+* A estrutura do Array ficará:
+* $calendario = array(
+*     0 => array( NULL, NULL, NULL, 1, 2, 3, 4),
+*     1 => array( 5, 6, 7, 8, 9, 10, 11),
+*     2 => array( 12, 13, 14, 15, 16, 17, 18),
+*     3 => array( 19, 20, 21, 22, 23, 24, 25),
+*     4 => array( 26, 27, 28, 29, 30, NULL, NULL)
+* );
+*/
+$calendario = array(
+    0 => array_fill(0, $pri_dia_sem, NULL)
+);
+ 
+$dia = 1;
+$semana = 0;
+$dia_semana = $pri_dia_sem;
+while ($dia < $num_dias) {
+    if ($dia_semana >= 7) {
+        $dia_semana = 0;
+        $semana++;
+    }
+ 
+    $calendario[$semana][$dia_semana] = $dia;
+ 
+    $dia++;
+    $dia_semana++;
+}
+$calendario[$semana] += array_fill($dia_semana, 6, NULL);
+ 
+print_r($calendario);
+?>
 	<section>
 	
 	<h1>
